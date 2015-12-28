d37902fbfddeb59677000eec2fe2f8454c4a423f
Stephane MARTINS <stmartin@e2r3p11.42.fr>
2015-12-28 17:47:24 +0100

    [update](check_diez)

--- srcs/check_diez.c
+++ srcs/check_diez.c
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/27 15:39:15 by stmartin          #+#    #+#             */
-/*   Updated: 2015/12/27 20:05:35 by stmartin         ###   ########.fr       */
+/*   Updated: 2015/12/28 17:45:37 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,29 @@ int		check_diez(t_coord **coord, int x, int y, int valid)
 {
 	while (coord[x] != NULL)
 	{
-		if ((coord[x][y].x == coord[x][y + 1].x && coord[x][y].y == coord[x]
-					[y + 1].y + 1) || (coord[x][y].x == coord[x][y + 1].x && 
-						coord[x][y].y == coord[x][y].y - 1))
+		if (y < 3 && coord[x][y].x == coord[x][y + 1].x && 
+				coord[x][y].y == coord[x][y + 1].y + 1)
 			valid++;
-		if ((coord[x][y].y == coord[x][y + 1].y && coord[x][y].x == coord[x]
-					[y].x + 1) || (coord[x][y].y == coord[x][y + 1].y && 
-						coord[x][y].x == coord[x][y + 1].x - 1))
+		if (y < 3 && coord[x][y].x == coord[x][y + 1].x && 
+				coord[x][y].y == coord[x][y + 1].y - 1)
+			valid++;
+		if (y > 0 && coord[x][y].x == coord[x][y - 1].x && 
+				coord[x][y].y == coord[x][y - 1].y - 1)
+			valid++;
+		if (y > 0 && coord[x][y].x == coord[x][y - 1].x &&
+				coord[x][y].y == coord[x][y - 1].y + 1)
+			valid++;
+		if (y < 3 && coord[x][y].y == coord[x][y + 1].y && 
+				coord[x][y].x == coord[x][y + 1].x + 1)
+			valid++;
+		if (y < 3 && coord[x][y].y == coord[x][y + 1].y && 
+				coord[x][y].x == coord[x][y - 1].x - 1)
+			valid++;
+		if (y > 0 && coord[x][y].y == coord[x][y - 1].y && 
+				coord[x][y].x == coord[x][y - 1].x - 1)
+			valid++;
+		if (y > 0 && coord[x][y].y == coord[x][y - 1].y &&
+				coord[x][y]. x == coord[x][y - 1].x + 1)
 			valid++;
 		y++;
 		if (y == 4)
@@ -31,6 +47,7 @@ int		check_diez(t_coord **coord, int x, int y, int valid)
 			x++;
 		}
 	}
+	printf("x = %d et y = %d octet suivant x = %d et y = %d\n", coord[0][0].x, coord[0][0].y, coord[0][1].x, coord[0][1].y);
 	printf("valid = %d", valid);
 	if (valid == x * 6)
 		return (1);
