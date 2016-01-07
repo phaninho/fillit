cf001da62404ae5572c771591553e670a903705a
phaninho <phaninho@live.fr>
2016-01-07 12:48:46 +0100

    fix merge

---
+++ includes/libit.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libit.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/22 17:45:57 by stmartin          #+#    #+#             */
+/*   Updated: 2015/12/27 20:02:43 by stmartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBIT_H
+# define LIBIT_H
+
+# include "libft.h"
+# include <fcntl.h>
+#include <stdlib.h>
+
+typedef struct		s_coord
+{
+	int	x;
+	int	y;
+}					t_coord;
+
+int					checkbuff(char *buff);
+int					check_valid(char *buff);
+t_coord				**add_coord(char *buff, size_t i, int x, int y);
+char				*fill_buff(int fd);
+int					check_diez(t_coord **coord, int x, int y, int valid);
+t_coord				**init_tet(t_coord **coord, int x, int y);
+#endif
