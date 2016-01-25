5c3c83711d21f39ddb4b21b9c0ed82ea0cf1fd0f
phaninho <phaninho@live.fr>
2016-01-25 21:31:22 +0100

    add clear_coord dans fillit/srcs dir

--- srcs/clean_memory.c
+++ srcs/clean_memory.c
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/24 17:44:13 by stmartin          #+#    #+#             */
-/*   Updated: 2016/01/25 19:39:24 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/01/25 20:46:34 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void		del_coord(t_test **coord)
 	int		i;
 
 	i = 0;
-	while (coord[i])
+	while (coord && coord[i])
 		free(coord[i++]);
 	free(coord);
 }
@@ -37,12 +37,13 @@ int		main()
 	int	i = 0;
 	int	j = 0;
 	t_test	**coord;
+
 	if (!(coord = (t_test**)malloc(sizeof(t_test*) * 3)))
 		exit (0);
 	coord[2] = NULL;
 	while (i < 2)
 	{
-		if (!(coord[i] = (t_test *)malloc(sizeof(t_test) * 2)))
+		if (!(coord[i] = (t_test *)malloc(sizeof(t_test) * 3)))
 			exit (0);
 		i++;
 	}
@@ -51,13 +52,17 @@ int		main()
 	coord[0][0].y = '#';
 	coord[0][1].x = '#';
 	coord[0][1].y = '#';
+	coord[0][2].x = 'A';
+	coord[0][2].y = 'A';
 	coord[1][0].x = '#';
 	coord[1][0].y = '#';
 	coord[1][1].x = '#';
 	coord[1][1].y = '#';
-	while (j < 2)
+	coord[1][2].x = 'B';
+	coord[1][2].y = 'B';
+	while (coord[j])
 	{
-		while (i < 2)
+		while (i < 3)
 		{
 			printf("%c%c\n",coord[j][i].x, coord[j][i].y);
 			i++;
